aceea7462854036cb0ca0cfd6b605bcbe2422195
Mitchell M <mitchellmclin@gmail.com>
2023-10-25 16:01:35 +0100

    5 create and set up mongodb (#9)
    * Add certificate to gitignore
    
    * Create repository service for health check
    
    * Use secrets.properties
    
    * Create MongoDB health check endpoint

--- src/main/java/tech/mclinton/fin/FinApplication.java
+++ src/main/java/tech/mclinton/fin/FinApplication.java
@@ -1,10 +1,18 @@
 package tech.mclinton.fin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import tech.mclinton.fin.repository.HealthRepository;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class FinApplication {
+
+    @Autowired
+    HealthRepository healthRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(FinApplication.class, args);
--- src/main/java/tech/mclinton/fin/api/health/HealthController.java
+++ src/main/java/tech/mclinton/fin/api/health/HealthController.java
@@ -1,17 +1,37 @@
 package tech.mclinton.fin.api.health;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tech.mclinton.fin.service.HealthService;
+
 @RestController("HealthController")
 @CrossOrigin
 @RequestMapping("/api/health")
 public class HealthController {
+    private final HealthService healthService;
+
+    public HealthController(@Qualifier("healthDatabase") HealthService healthService) {
+        this.healthService = healthService;
+    }
+
     @GetMapping
     public ResponseEntity<String> HealthHandler() {
         return ResponseEntity.ok("Hello, Fin!");
+    }
+
+    @GetMapping("/mongo")
+    public ResponseEntity<String> MongoHealthHandler() {
+        String checkResponse = healthService.makeHealthCheck();
+
+        if (checkResponse == null) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        return ResponseEntity.ok(checkResponse);
     }
 }
